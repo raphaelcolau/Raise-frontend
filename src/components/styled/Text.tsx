a9be194524425c4a836f990a6ea87d78b60ef6ef
Raphael C <polyster20@gmail.com>
2023-12-20 20:20:53 +0100

    Update Text.tsx

--- src/components/styled/Text.tsx
+++ src/components/styled/Text.tsx
@@ -3,12 +3,16 @@ import { withTheme } from "react-native-paper";
 import { WithThemeProps } from "../type/types";
 import { Text } from 'react-native-paper';
 import { VariantProp } from "react-native-paper/lib/typescript/components/Typography/types";
+import { TextProps } from "react-native";
 
-interface TextWithThemeProps extends WithThemeProps {
-    variant?: VariantProp<never>;
+interface TextWithThemeProps extends TextProps {
+    theme: any;
+    style?: any;
+    children?: any;
+    variant?: VariantProp<any>;
 }
 
-const TextWithTheme = ({ theme, children, variant, style }: TextWithThemeProps) => {
+const TextWithTheme = ({ theme, children, style, variant, ...props }: TextWithThemeProps) => {
     const { colors } = theme;
 
     return (
