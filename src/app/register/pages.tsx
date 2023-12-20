0e0d83f94f50bacae80cc537e02bd8cd15cffd1d
Raphael C <polyster20@gmail.com>
2023-12-20 21:15:15 +0100

    Update pages.tsx
    Register page done

--- src/app/register/pages.tsx
+++ src/app/register/pages.tsx
@@ -5,14 +5,17 @@ import { useTheme } from '@react-navigation/native';
 import View from '../../components/styled/View';
 import Text from '../../components/styled/Text';
 import Button from '../../components/styled/Button';
+import Chip from '../../components/styled/Chip';
 import StyledTextInput from '../../components/styled/TextInput';
-
+import { GENDER } from '../../components/type/types';
 
 export default function Register({ navigation }: { navigation: any}) {
     const theme = useTheme();
     const [secureText, setSecureText] = React.useState(true);
     const [username, setUsername] = React.useState('');
+    const [email, setEmail] = React.useState('');
     const [password, setPassword] = React.useState('');
+    const [gender, setGender] = React.useState<GENDER>(GENDER.NOT_SPECIFIED);
 
     const styles = StyleSheet.create({
         container: {
@@ -31,7 +34,7 @@ export default function Register({ navigation }: { navigation: any}) {
             width: 135,
             height: 52,
         },
-        title: {
+        bold: {
             fontWeight: 'bold',
         },
         subtitle: {
@@ -43,10 +46,6 @@ export default function Register({ navigation }: { navigation: any}) {
             display: 'flex',
             flexDirection: 'column',
             gap: 22,
-        },
-        unableToConnect: {
-            textAlign: 'right',
-            textDecorationLine: 'underline',
         },
     });
 
@@ -66,10 +65,44 @@ export default function Register({ navigation }: { navigation: any}) {
             </View>
 
             <View>
-                <Text variant="displaySmall" style={styles.title}>Vous revoilà!</Text>
-                <Text variant="bodyLarge" style={styles.subtitle}>Entrez votre nom d'utilisateur et mot de passe</Text>
+                <Text variant="displaySmall" style={styles.bold}>Bienvenue!</Text>
+                <Text variant="bodyLarge" style={styles.subtitle}>Dites-nous qui vous êtes avant de commencer</Text>
             </View>
 
+            <View>
+                <Text variant="titleSmall" style={styles.bold}>Vous êtes</Text>
+                <View
+                    style={{
+                        display: 'flex',
+                        flexDirection: 'row',
+                        justifyContent: 'space-between',
+                        gap: 10,
+                        marginTop: 10,
+                    }}
+                >
+                    <Chip 
+                        icon="gender-male" 
+                        selected={gender === GENDER.MAN}
+                        onPress={() => setGender(GENDER.MAN)}
+                    >
+                        Homme
+                    </Chip>
+                    <Chip 
+                        icon="gender-female" 
+                        selected={gender === GENDER.WOMAN}
+                        onPress={() => setGender(GENDER.WOMAN)}
+                    >
+                        Femme
+                    </Chip>
+                    <Chip 
+                        icon="helicopter" 
+                        selected={gender === GENDER.NOT_SPECIFIED}
+                        onPress={() => setGender(GENDER.NOT_SPECIFIED)}
+                    >
+                        Autres
+                    </Chip>
+                </View>
+            </View>
 
             <View style={styles.formInput}>
                 <StyledTextInput 
@@ -79,6 +112,13 @@ export default function Register({ navigation }: { navigation: any}) {
                     onChangeText={(text) => setUsername(text)}
                 />
                 <StyledTextInput 
+                    label="E-mail"
+                    placeholder="Entrez votre e-mail"
+                    autoComplete='email'
+                    onChangeText={(text) => setEmail(text)}
+                />
+
+                <StyledTextInput 
                     label="Mot de passe"
                     placeholder="Entrez votre mot de passe"
                     autoComplete='password'
@@ -86,12 +126,11 @@ export default function Register({ navigation }: { navigation: any}) {
                     onChangeText={(text) => setPassword(text)}
                     right={<TextInput.Icon icon={secureText ? "eye" : "eye-off"} onPress={() => handleSecureText()} />}
                 />
-                <Text style={styles.unableToConnect} variant="bodySmall">Je n'arrive pas à me connecter</Text>
             </View>
 
             <View>
                 <Button onPress={() => validForm()}>
-                    Se connecter
+                    S'inscrire
                 </Button>
             </View>
 
