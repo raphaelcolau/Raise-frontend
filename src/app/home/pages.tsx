cf5fe45cf65b948293dcd41bbe2336ef65f35cbb
Raphael C <polyster20@gmail.com>
2023-12-21 15:18:01 +0100

    Working on Weekly calendar

--- src/app/home/pages.tsx
+++ src/app/home/pages.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, FlatList, TouchableOpacity } from 'react-native';
 import { Button, Icon, IconButton, Surface, useTheme, Text } from 'react-native-paper';
 import StyledView from '../../components/styled/View';
 import Header from './_components/Header';
@@ -35,11 +35,50 @@ function Day({date, selected}: {date: Date, selected?: boolean}) {
             <Text variant="bodyLarge" style={{...styles.text, fontWeight: 'bold'}}>{day}</Text>
         </View>
     )
+}
 
+/**
+ * Renders a week component.
+ * @param {number} week - The week number. (0 = current week, 1 = next week, etc.)
+ * @returns {JSX.Element} - The rendered week component.
+ */
+function Week({week, daySelected, setSelectedDay}: {week: number, daySelected?: Date, setSelectedDay: Function}) {
+    const dayOfTheWeek = new Date().getDay() - 1;
+    const styles = StyleSheet.create({
+        dateContainer: {
+            flexDirection: 'row',
+            alignItems: 'center',
+            width: 344,
+            justifyContent: 'space-between',
+        },
+    });
+
+    return (
+        <View>
+            <View style={styles.dateContainer}>
+                {[0, 1, 2, 3, 4, 5, 6].map((index) => (
+                    <TouchableOpacity
+                        key={index}
+                        onPress={() => setSelectedDay(new Date(new Date().setDate(new Date().getDate() + (index - dayOfTheWeek) - (-week * 7))))}
+                    >
+                        <Day
+                            selected={daySelected?.getDate() === new Date(new Date().setDate(new Date().getDate() + (index - dayOfTheWeek) - (-week * 7))).getDate()}
+                            date={new Date(new Date().setDate(new Date().getDate() + (index - dayOfTheWeek) - (-week * 7)))}
+                        />
+                    </TouchableOpacity>
+                ))}
+            </View>
+        </View>
+    )
 }
 
 function WeeklyCalendar() {
     const theme = useTheme();
+    const weekContainerWidth = 344;
+    const flatListRef = React.useRef(null);
+    const [scrollIndex, setScrollIndex] = React.useState(2);
+    const [selectedDay, setSelectedDay] = React.useState(new Date());
+    const [displayedMonthAndYear, setDisplayedMonthAndYear] = React.useState(currentMonthAndYear());
 
     const styles = StyleSheet.create({
         container: {
@@ -72,15 +111,16 @@ function WeeklyCalendar() {
             flexDirection: 'row',
             alignItems: 'center',
         },
-        dateContainer: {
+        scrollView: {
+            width: '100%',
+            overflow: 'hidden',
+            display: 'flex',
             flexDirection: 'row',
-            alignItems: 'center',
-            justifyContent: 'space-between',
         },
     });
 
-    function currentMonthAndYear() {
-        const date = new Date();
+    function currentMonthAndYear(): string {
+        const date = selectedDay;
         const month = date.toLocaleString('fr-FR', { month: 'long' });
         const year = date.getFullYear();
 
@@ -96,30 +136,53 @@ function WeeklyCalendar() {
         }}
     />;
 
+    React.useEffect(() => {
+        setDisplayedMonthAndYear(currentMonthAndYear());
+    }, [selectedDay]);
+
+    // const handleScroll = (event: any) => {
+    //     const newScrollIndex = Math.round(event.nativeEvent.contentOffset.x / 344);
+    //     if (newScrollIndex === scrollIndex) return;
+    //     setScrollIndex(newScrollIndex);
+    //     if (flatListRef.current === null) return;
+    //     (flatListRef.current as any).scrollToIndex({index: newScrollIndex, animated: true})
+    // };
+
+    const handleMove = (direction: 'forward' | 'backward') => {
+        const move = direction === 'forward' ? 1 : -1;
+        if (scrollIndex + move < 0 || scrollIndex + move > 4) return;
+        setScrollIndex(scrollIndex + move)
+        if (flatListRef.current === null) return;
+        (flatListRef.current as any).scrollToIndex({index: (scrollIndex + move), animated: true})
+    };
+
     return (
         <View style={styles.container}>
             <Surface elevation={1} style={styles.surfaceContainer}>
                 
                 <View style={styles.topContainer}>
 
-                    <StyledText style={{textTransform: 'capitalize', color: theme.colors.onSurface}} >{currentMonthAndYear()}</StyledText>
+                    <StyledText style={{textTransform: 'capitalize', color: theme.colors.onSurface}} >{displayedMonthAndYear}</StyledText>
 
                     <View style={styles.buttons}>
-                        <StyledIconButton icon="chevron-left" onPress={() => console.log('chevron gauche')}/>
-                        <StyledIconButton icon="chevron-right" onPress={() => console.log('chevron droit')}/>
+                        <StyledIconButton icon="chevron-left" onPress={() => handleMove('backward')}/>
+                        <StyledIconButton icon="chevron-right" onPress={() => handleMove('forward')}/>
                     </View>
 
                 </View>
 
-                <View style={styles.dateContainer}>
-                    <Day selected date={new Date()} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 1))} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 2))} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 3))} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 4))} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 5))} />
-                    <Day date={new Date(new Date().setDate(new Date().getDate() + 6))} />
-                </View>
+                <FlatList
+                    horizontal
+                    showsHorizontalScrollIndicator={false}
+                    style={styles.scrollView}
+                    data={[-2, -1, 0, 1, 2]}
+                    ref={flatListRef}
+                    keyExtractor={(item) => item.toString()}
+                    renderItem={({item}) => <Week week={item} setSelectedDay={setSelectedDay} daySelected={selectedDay} />}
+                    initialScrollIndex={scrollIndex}
+                    // onScroll={handleScroll}
+                    scrollEventThrottle={16}
+                />
 
             </Surface>
         </View>
