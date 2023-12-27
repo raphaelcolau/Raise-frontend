825829e670d92309e78c084214158348a8b5a0ea
Raphael C <polyster20@gmail.com>
2023-12-27 17:11:54 +0100

    Update userTrainings.ts

--- src/adapters/userTrainings.ts
+++ src/adapters/userTrainings.ts
@@ -1,10 +1,12 @@
 import axios, { AxiosError } from "axios";
 import { API_URL } from "./api";
+import { Training } from "../components/type/types";
 
 export async function getUserTrainings(selectedDay: Date) {
     try {
         const response = await axios.get(`${API_URL}/api/training/validate-training-day/${selectedDay.toISOString()}`);
         const data = response.data;
+        const trainings: Array<Training> = data.data;
 
         if (data.success) {
             return data.data;
