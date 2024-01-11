e42dfb6a8245f6f4206642657f9241581a320ee9
Raphael C <polyster20@gmail.com>
2024-01-11 19:26:01 +0100

    Fix file extension

--- src/adapters/forgotPassword.ts
+++ src/adapters/forgotPassword.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 import { API_URL } from './api';
-import { CreateTrainingProps } from '../components/type/types';
 
 export const forgotPassword = async (email: string) => {
   const response = await axios.post(`${API_URL}/api/auth/forgot-password`, {
