f5a96a69945d202d516fee1227fdc1412d7efa02
Raphael C <polyster20@gmail.com>
2024-01-15 22:20:50 +0100

    Display done

--- src/app/performManuallyTrainingSession/pages.tsx
+++ src/app/performManuallyTrainingSession/pages.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Pressable, StyleSheet, View } from 'react-native';
-import { Icon, Text, useTheme } from 'react-native-paper';
+import { Button, Icon, Text, useTheme } from 'react-native-paper';
 import HeaderSubPage from '../../components/headerSubPage/HeaderSubPage';
 import { Training, Exercise } from '../../components/type/types';
 import StyledButton from '../../components/styled/Button';
@@ -13,22 +13,70 @@ function ExerciseMenu({isActive, exercise, setSelected}: {isActive: boolean, exe
         container: {
             position: 'relative',
             width: '100%',
-            height: 70,
             padding: 10,
+            backgroundColor: colors.surface,
+            borderRadius: 15,
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+            gap: 15,
+        },
+        header: {
             display: 'flex',
             flexDirection: 'row',
             justifyContent: 'space-between',
             alignItems: 'center',
-            backgroundColor: colors.surface,
-            borderRadius: 15,
-        },
+            width: '100%',
+        }
     });
 
     return (
         <Pressable onPress={() => setSelected(exercise.exerciseId)}>
             <View style={styles.container}>
-                <Text variant='titleSmall'>{exercise.exerciseName}</Text>
-                <Icon source={ isActive ? 'menu-up' : 'menu-down'} size={30} color={colors.onSurface} />
+
+                <View style={styles.header}>
+                    <View style={{padding: 7}}>
+
+                        <View style={{display: 'flex', flexDirection: 'row', gap: 6}}>
+                            <Text variant='titleMedium'>{exercise.exerciseName}</Text>
+                            <Text variant='titleMedium' style={{color: colors.primary}}>#{exercise.exerciseId}</Text>
+                        </View>
+
+                        <Text variant='bodyMedium'>{exercise.series.length} série{exercise.series.length > 1 && 's'}</Text>
+
+                    </View>
+
+                    <Icon source={ isActive ? 'menu-up' : 'menu-down'} size={30} color={colors.onSurface} />
+                </View>
+
+                {isActive &&
+                    <View style={{padding: 5, display: 'flex', flexDirection: 'column', gap: 10}}>
+                        <View style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                            <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}> </Text>
+                            <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>Reps</Text>
+                            <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>Charge</Text>
+                            <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>Repo</Text>
+                        </View>
+
+                        {exercise.series.map((serie) => (
+                            <View key={serie.id} style={{display: 'flex', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center'}}>
+                                <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}> ● Série {serie.id < 10 ? `0${serie.id}` : serie.id}</Text>
+                                <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>{serie.repsCount}</Text>
+                                <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>{serie.weight}</Text>
+                                <Text variant='bodyLarge' style={{flex: 1, textAlign: 'center'}}>{serie.restTime}</Text>
+                            </View>
+                        ))}
+                        
+                    </View>
+                }
+
+                {isActive &&
+                    <View style={{display: 'flex', flexDirection: 'row-reverse'}}>
+                        <StyledButton>Modifier</StyledButton>
+                        <Button textColor={colors.onSurface}>Passer l'exercice</Button>
+                    </View>
+                }
+
             </View>
         </Pressable>
     )
@@ -77,7 +125,7 @@ export default function PerformManuallyTrainingSession({ navigation, route }: { 
                     <Text variant='titleMedium'>Séance {training.trainingName}</Text>
                 </View>
 
-                <View style={{flex: 1}}>
+                <View style={{flex: 1, display: 'flex', flexDirection: 'column', gap: 15}}>
                     {training.trainingExercises.map((exercise: Exercise) => (<ExerciseMenu key={exercise.exerciseId} isActive={selected === exercise.exerciseId} setSelected={handleSetSelected} exercise={exercise} />))}
                 </View>
 
